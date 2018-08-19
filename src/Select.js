49f4f9f273e3811b5af7bc0793e9a0314d870637
behemothoz <behemothoz@yandex.ru>
2018-08-19 16:03:06 +0300

    fix style

--- src/Select.js
+++ src/Select.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import './Select.css';
 
 class Select extends Component {
+  static defaultProps = {
+    items: [1, 2, 3]
+  }
+
   constructor(props) {
     super(props);
   }
 
+  renderItemsContainer() {
+    return (
+      <div className="select-items-container">
+        <ul className="select-items-list">
+          { this.props.items.map(item => <li className="select-items-list-item">{item}</li>) }
+        </ul>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className="select-root">
-        <div className="select-value">Default value</div>
-        <button className="select-arrow-button">
-          <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M7 10l5 5 5-5z"></path>
-          </svg>
-        </button>
-      </div>
+      <React.Fragment>
+        <div className="select-root">
+          <div className="select-value">Default value</div>
+          <button className="select-arrow-button">
+            <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M7 10l5 5 5-5z"></path>
+            </svg>
+          </button>
+        </div>
+
+        { this.renderItemsContainer() }
+      </React.Fragment>
     )
   }
 }
